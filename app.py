5f4eda907a591a61fddaeea23e8e108b7076dc4a
Bartek_makowski <mako19895@gmail.com>
2021-02-07 23:45:17 +0000

    app.py add 404 and 500 error handlers

--- app.py
+++ app.py
@@ -184,6 +184,26 @@ def delete_review(feedback_id):
     return redirect(url_for('myaccount'))
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Renders error.html with 404 message
+    """
+    error_message = str(error)
+    return render_template('pages/error.html',
+                        error_message=error_message), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    """
+    Renders error.html with 500 message
+    """
+    error_message = str(error)
+    return render_template('pages/error.html',
+                        error_message=error_message), 500
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
